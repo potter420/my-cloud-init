7dadd8451cd3ba75029b211e5ccdcb62d14b7c10
thanhmt0612 <thanhmt0612@gmail.com>
2020-05-19 16:06:10 +0700

    change dir

--- init.sh
+++ init.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
+cd /srv/
 # Setup nginx
 bash nginx/nginx.sh
 sudo nginx -s reload
 # Setup wstunnel
 apt install unzip
+
 wget https://github.com/erebe/wstunnel/releases/download/v3.0/wstunnel-x64-linux.zip 
 unzip wstunnel-x64-linux.zip
-sudo chmod 755 wstunnel
-sudo ln -s $PWD/wstunnel /usr/bin/wstunnel
+sudo chmod 755 /srv/wstunnel
+sudo ln -s /srv/wstunnel /usr/bin/wstunnel
 sudo cp wstunnel.service /etc/systemd/system/
 systemctl enable wstunnel && systemctl restart wstunnel
 # Setup Openvpn
+
+cd /home/ubuntu
