7af3e0975054f9d9745e07230ffdc63169f54ef7
thanhmt0612 <thanhmt0612@gmail.com>
2020-05-19 16:02:01 +0700

    added wstunnel

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup nginx
+bash nginx/nginx.sh
+sudo nginx -s reload
+# Setup wstunnel
+apt install unzip
+wget https://github.com/erebe/wstunnel/releases/download/v3.0/wstunnel-x64-linux.zip 
+unzip wstunnel-x64-linux.zip
+sudo chmod 755 wstunnel
+ln -s $PWD/wstunnel /usr/bin/wstunnel
+sudo cp wstunnel.service /etc/systemd/system/
+systemctl enable wstunnel && systemctl restart wstunnel
+# Setup Openvpn
