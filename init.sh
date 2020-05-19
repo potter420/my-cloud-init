2372b8f4bec3ee04cf4902c27a91849e0f56ddfb
thanhmt0612 <thanhmt0612@gmail.com>
2020-05-19 16:03:04 +0700

    fixed permission

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ apt install unzip
 wget https://github.com/erebe/wstunnel/releases/download/v3.0/wstunnel-x64-linux.zip 
 unzip wstunnel-x64-linux.zip
 sudo chmod 755 wstunnel
-ln -s $PWD/wstunnel /usr/bin/wstunnel
+sudo ln -s $PWD/wstunnel /usr/bin/wstunnel
 sudo cp wstunnel.service /etc/systemd/system/
 systemctl enable wstunnel && systemctl restart wstunnel
 # Setup Openvpn
